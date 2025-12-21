c78e963cfbce51b8109bc6064e900487bff794dd
Jonathan Mansell <mansellj2013@gmail.com>
2025-12-21 10:50:06 -0500

    Increase body parser limits to handle large form submissions

--- server.js
+++ server.js
@@ -50,8 +50,8 @@ app.set("views", path.join(__dirname, "views"));
 // Middleware
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/app", express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
+app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 
 // Request logging middleware
